df0d257d001e7fae97a424111fce6503561fb69c
mssaldy <33184266+mssaldy@users.noreply.github.com>
2018-12-23 23:00:08 +0700

    Data Binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Saldy',
-    job: 'Web Developer'
+    job: 'Web Developer',
+    website: 'http://www.thenetninja.co.uk',
+    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
   },
   methods: {
     greet: function(time) {
